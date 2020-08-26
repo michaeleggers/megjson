ffed13b2c905e11730e5a1a41a32aeb67944abf1
Michael Eggers <eggers@hm.edu>
2020-08-26 17:25:56 +0200

    init commit of json parser

---
+++ build.bat
@@ -0,0 +1,27 @@
+
+@echo off
+
+
+
+set flags_debug=   -std=c11 -Wall -Wextra -pedantic-errors -fextended-identifiers -g -D DEBUG
+set flags_release=   -std=c11 -Wall -Wextra -pedantic-errors -fextended-identifiers
+
+set clang_flags_debug= /Z7 /DDEBUG /W4 /WX /MDd -Qunused-arguments
+set clang_flags_debug_easy= /Z7 /DDEBUG /W4 /MDd -Qunused-arguments -Wno-unused-variable
+
+
+pushd "%~dp0"
+mkdir build
+pushd build
+
+REM gcc %flags_debug%   ..\src\main.c -o main_gcc_dbg
+REM gcc %flags_release% ..\src\main.c -o main_gcc_release
+
+REM clang-cl %clang_flags_debug%  ..\src\main.c -o main_clang_dbg
+clang-cl %clang_flags_debug_easy% ..\src\main.c -o main_clang_dbg_easy.exe
+
+
+popd
+popd
+
+
